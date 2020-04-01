59a2383e876776a2f397fea53d04a0820f758af6
lettsdom0 <lettsdom0@gmail.com>
2020-04-01 16:34:36 -0400

    Gor started on movement
    I added a new script for player movement when on the motherboard/skateboard. Unsure of whether or not to include movement when off of the board or not, but i'm doing the motherboard/skateboard stuff first. Lemme know if anything needs clarified!

--- CyberskateProject/Blank Unity Project/Assets/Scripts/Movement/SkateBoardMovement.cs
+++ CyberskateProject/Blank Unity Project/Assets/Scripts/Movement/SkateBoardMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SkateBoardMovement : MonoBehaviour
 {
     public bool falling;
+    public float pressTime = 0.0f;
     public float moveSpeed;
     public float jumpHeight;
     public int addedForwardMomentum;
@@ -19,15 +20,33 @@ public class SkateBoardMovement : MonoBehaviour
     private void Accelarate()
     {
         //Player character Kicks the ground to boost their speed
-        //in script this adds some speed to the player character so they can moove faster
+        //in script this adds some speed to the player character so they can move faster
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            pressTime += Time.deltaTime;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            moveSpeed += pressTime;
+            pressTime = 0.0f;
+        }
 
     }
 
-    private void leaningForce()
+    private void lean()
     {
         //player leans to manuver their motherboard left and right
         //in script this will make it so if the player presses the left button for example: 
         // the board will slowly but surely turn left.
+
+    }
+
+    private void hitTheBreaks()
+    {
+        //player stops the skateborad by slamming their foot on the ground
+        //in script this will make it so momentum is stopped.
+
 
     }
 
