5d9d1e32cf3226270b661412d56db504375db323
etogashi23 <60116796+etogashi23@users.noreply.github.com>
2020-04-06 20:39:40 -0400

    Started working on Watching system

---
+++ CyberskateProject/Blank Unity Project/Assets/Scripts/Watching System/Sight.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sight : MonoBehaviour
+{
+    public bool playerInSight;
+
+    private List<GameObject> players;
+
+    void Start()
+    {
+        players = new List<GameObject>(GameObject.FindGameObjectsWithTag("Player")); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnTriggerStay(Collider other)
+    {
+        if (players.Contains(other.gameObject))
+        {
+            playerInSight = true;
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (players.Contains(other.gameObject))
+            playerInSight = false;
+    }
+}
